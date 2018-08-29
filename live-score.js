17c37e2cf177430130a94cee36c06928c50d6b16
Chem <chembeer.agaton@gmail.com>
2018-08-29 11:51:52 +0800

    variable date

--- live-score.js
+++ live-score.js
@@ -1,6 +1,26 @@
 var originalURL = "http://livescore-api.com/api-client/scores/live.json?key=gop822jNO6z33GAq&secret=iglZeHtSNGjCucWKAmTCocqdiZhlGSMY";
 var queryURL = "https://cors-anywhere.herokuapp.com/" + originalURL;
 var matchs = {};
+
+var add_minutes =  function (dt, minutes) {
+	return new Date(dt.getTime() + minutes*60000);
+}
+var addZero = function (i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+var addTimeToDate = function (thedate) {
+	var matchEnded = new Date((add_minutes(new Date(thedate), 420).toString()));
+	var getDay = addZero(matchEnded.getDate());
+	var getMonth = addZero(matchEnded.getMonth() + 1);
+	var getYear = matchEnded.getFullYear();
+	var getHours = addZero(matchEnded.getHours());
+	var getMin = addZero(matchEnded.getMinutes());
+	var getSec = addZero(matchEnded.getSeconds());
+	return (getYear+'-'+getMonth+'-'+getDay+' '+getHours+':'+getMin+':'+getSec);
+}
 $.ajax({
 	url: queryURL,
 	method: "GET",
@@ -21,13 +41,18 @@ $.ajax({
 	$.each(matchObj, function(i, value){
 		$('#live-score-container tbody').append('<tr class="league-title"><td></td><td>'+i+'</td><td></td><td></td></tr>')
 		$.each(this, function(){
+			var matchEnd = addTimeToDate(this.last_changed);
+			var matchStart = addTimeToDate(this.added);
+			var currentDate = addTimeToDate(new Date());
+			console.log(currentDate);
+			
 			var matchWinner;
 				score = this.score.split('-');
 				homeScore = parseInt(score[0]);
 				awayScore = parseInt(score[1]);
 			
-			if(homeScore > awayScore){matchWinner = this.home_name;} 
-			else if (homeScore < awayScore){matchWinner = this.away_name;} 
+			if(homeScore > awayScore){matchWinner = this.home_name;}
+			else if (homeScore < awayScore){matchWinner = this.away_name;}
 			else {matchWinner = 'Draw!';};
 			
 			$('#live-score-container tbody').append('<tr class="match-single ">\
@@ -38,7 +63,7 @@ $.ajax({
 			</tr>');
 		});
 	});
-
+	console.log(matchObj);
 }).fail(function(jqXHR, textStatus) {
 	console.error(textStatus)
 });
